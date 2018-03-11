d14b576803174bd4f049e7bdd4354b940fc1320e
ASClay <35178271+ASClay@users.noreply.github.com>
2018-03-10 18:39:40 -0600

    9

---
+++ cit230/assignments/lesson-9/scripts/currentdate.js
@@ -0,0 +1,15 @@
+
+function currentdate() {
+    var mydate = new Date(); 
+    var year = mydate.getYear(); 
+    var day = mydate.getDay(); 
+    var month = mydate.getMonth(); 
+    var daym = mydate.getDate(); 
+    var dayarray = new Array("Sunday,","Monday,","Tuesday,","Wednesday,","Thursday,","Friday,","Saturday,"); var montharray = new Array("January","February","March","April","May","June","July","August","September","October","November","December"); 
+    var myClock = document.getElementById("currentdate");
+    myClock.textContent = dayarray[day]+ " " +daym+ " " +montharray[month]+ " 2018";
+    myClock.innerText = dayarray[day]+ " " +daym+ " " +montharray[month]+ " 2018";
+    
+    setTimeout("currentdate()", 1000);
+}
+currentdate();
