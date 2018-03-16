847640e427d4f2f7d77a18281383b69b6447ac36
ASClay <35178271+ASClay@users.noreply.github.com>
2018-03-16 10:20:42 -0500

    9

--- cit230/assignments/lesson-9/scripts/franklinW.js
+++ cit230/assignments/lesson-9/scripts/franklinW.js
@@ -4,13 +4,13 @@ weatherObject.open('GET', 'https://api.wunderground.com/api/c9e984e1026221f7/con
 weatherObject.send();
 
 weatherObject.onload = function () {
-    var franklinWeather = JSON.parse(weatherObject.responseText);
-    console.log(franklinWeather);
-    document.getElementById('currentObs').innerHTML = franklinWeather.current_observation.weather;
-    document.getElementById('currentTemp').innerHTML = franklinWeather.current_observation.temp_f;
-    document.getElementById('windSpeed').innerHTML = franklinWeather.current_observation.wind_string;
-    document.getElementById('humid').innerHTML = franklinWeather.current_observation.relative_humidity;
-    document.getElementById('w_icon').src = franklinWeather.current_observation.icon_url;
+    var townWeather = JSON.parse(weatherObject.responseText);
+    console.log(townWeather);
+    document.getElementById('currentObs').innerHTML = townWeather.current_observation.weather;
+    document.getElementById('currentTemp').innerHTML = townWeather.current_observation.temp_f;
+    document.getElementById('windSpeed').innerHTML = townWeather.current_observation.wind_string;
+    document.getElementById('humid').innerHTML = townWeather.current_observation.relative_humidity;
+    document.getElementById('w_icon').src = townWeather.current_observation.icon_url;
 }
 
 
@@ -21,22 +21,22 @@ forecastObject.open('GET', 'https://api.wunderground.com/api/c9e984e1026221f7/fo
 forecastObject.send();
 
 forecastObject.onload = function () {
-    var franklinForecast = JSON.parse(forecastObject.responseText);
-    console.log(franklinForecast);
-    document.getElementById('forecast1').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["0"].fcttext;
-    document.getElementById('w_icon1').src = franklinForecast.forecast.txt_forecast.forecastday["0"].icon_url;
+    var townForecast = JSON.parse(forecastObject.responseText);
+    console.log(townForecast);
+    document.getElementById('forecast1').innerHTML = townForecast.forecast.txt_forecast.forecastday["0"].fcttext;
+    document.getElementById('w_icon1').src = townForecast.forecast.txt_forecast.forecastday["0"].icon_url;
     
-    document.getElementById('forecast2').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["1"].fcttext;
-    document.getElementById('w_icon2').src = franklinForecast.forecast.txt_forecast.forecastday["1"].icon_url;
+    document.getElementById('forecast2').innerHTML = townForecast.forecast.txt_forecast.forecastday["1"].fcttext;
+    document.getElementById('w_icon2').src = townForecast.forecast.txt_forecast.forecastday["1"].icon_url;
     
-    document.getElementById('forecast3').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["2"].fcttext;
-    document.getElementById('w_icon3').src = franklinForecast.forecast.txt_forecast.forecastday["2"].icon_url;
+    document.getElementById('forecast3').innerHTML = townForecast.forecast.txt_forecast.forecastday["2"].fcttext;
+    document.getElementById('w_icon3').src = townForecast.forecast.txt_forecast.forecastday["2"].icon_url;
     
-    document.getElementById('forecast4').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["3"].fcttext;
-    document.getElementById('w_icon4').src = franklinForecast.forecast.txt_forecast.forecastday["3"].icon_url;
+    document.getElementById('forecast4').innerHTML = townForecast.forecast.txt_forecast.forecastday["3"].fcttext;
+    document.getElementById('w_icon4').src = townForecast.forecast.txt_forecast.forecastday["3"].icon_url;
     
-    document.getElementById('forecast5').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["4"].fcttext;
-    document.getElementById('w_icon5').src = franklinForecast.forecast.txt_forecast.forecastday["4"].icon_url;
+    document.getElementById('forecast5').innerHTML = townForecast.forecast.txt_forecast.forecastday["4"].fcttext;
+    document.getElementById('w_icon5').src = townForecast.forecast.txt_forecast.forecastday["4"].icon_url;
     
     
 }
--- cit230/assignments/lesson-9/scripts/greenW.js
+++ cit230/assignments/lesson-9/scripts/greenW.js
@@ -1,42 +1,42 @@
 var weatherObject = new XMLHttpRequest();
-weatherObject.open('GET', 'https://api.wunderground.com/api/c9e984e1026221f7/conditions/q/MN/Springfield.json', true);
+weatherObject.open('GET', 'https://api.wunderground.com/api/c9e984e1026221f7/conditions/q/TX/Greenville.json', true);
 
 weatherObject.send();
 
 weatherObject.onload = function () {
-    var franklinWeather = JSON.parse(weatherObject.responseText);
-    console.log(franklinWeather);
-    document.getElementById('currentObs').innerHTML = franklinWeather.current_observation.weather;
-    document.getElementById('currentTemp').innerHTML = franklinWeather.current_observation.temp_f;
-    document.getElementById('windSpeed').innerHTML = franklinWeather.current_observation.wind_string;
-    document.getElementById('humid').innerHTML = franklinWeather.current_observation.relative_humidity;
-    document.getElementById('w_icon').src = franklinWeather.current_observation.icon_url;
+    var townWeather = JSON.parse(weatherObject.responseText);
+    console.log(townWeather);
+    document.getElementById('currentObs').innerHTML = townWeather.current_observation.weather;
+    document.getElementById('currentTemp').innerHTML = townWeather.current_observation.temp_f;
+    document.getElementById('windSpeed').innerHTML = townWeather.current_observation.wind_string;
+    document.getElementById('humid').innerHTML = townWeather.current_observation.relative_humidity;
+    document.getElementById('w_icon').src = townWeather.current_observation.icon_url;
 }
 
 
 
 var forecastObject = new XMLHttpRequest();
-forecastObject.open('GET', 'https://api.wunderground.com/api/c9e984e1026221f7/forecast/q/MN/Springfield.json', true);
+forecastObject.open('GET', 'https://api.wunderground.com/api/c9e984e1026221f7/forecast/q/TX/Greenville.json', true);
 
 forecastObject.send();
 
 forecastObject.onload = function () {
-    var franklinForecast = JSON.parse(forecastObject.responseText);
-    console.log(franklinForecast);
-    document.getElementById('forecast1').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["0"].fcttext;
-    document.getElementById('w_icon1').src = franklinForecast.forecast.txt_forecast.forecastday["0"].icon_url;
+    var townForecast = JSON.parse(forecastObject.responseText);
+    console.log(townForecast);
+    document.getElementById('forecast1').innerHTML = townForecast.forecast.txt_forecast.forecastday["0"].fcttext;
+    document.getElementById('w_icon1').src = townForecast.forecast.txt_forecast.forecastday["0"].icon_url;
     
-    document.getElementById('forecast2').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["1"].fcttext;
-    document.getElementById('w_icon2').src = franklinForecast.forecast.txt_forecast.forecastday["1"].icon_url;
+    document.getElementById('forecast2').innerHTML = townForecast.forecast.txt_forecast.forecastday["1"].fcttext;
+    document.getElementById('w_icon2').src = townForecast.forecast.txt_forecast.forecastday["1"].icon_url;
     
-    document.getElementById('forecast3').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["2"].fcttext;
-    document.getElementById('w_icon3').src = franklinForecast.forecast.txt_forecast.forecastday["2"].icon_url;
+    document.getElementById('forecast3').innerHTML = townForecast.forecast.txt_forecast.forecastday["2"].fcttext;
+    document.getElementById('w_icon3').src = townForecast.forecast.txt_forecast.forecastday["2"].icon_url;
     
-    document.getElementById('forecast4').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["3"].fcttext;
-    document.getElementById('w_icon4').src = franklinForecast.forecast.txt_forecast.forecastday["3"].icon_url;
+    document.getElementById('forecast4').innerHTML = townForecast.forecast.txt_forecast.forecastday["3"].fcttext;
+    document.getElementById('w_icon4').src = townForecast.forecast.txt_forecast.forecastday["3"].icon_url;
     
-    document.getElementById('forecast5').innerHTML = franklinForecast.forecast.txt_forecast.forecastday["4"].fcttext;
-    document.getElementById('w_icon5').src = franklinForecast.forecast.txt_forecast.forecastday["4"].icon_url;
+    document.getElementById('forecast5').innerHTML = townForecast.forecast.txt_forecast.forecastday["4"].fcttext;
+    document.getElementById('w_icon5').src = townForecast.forecast.txt_forecast.forecastday["4"].icon_url;
     
     
 }
